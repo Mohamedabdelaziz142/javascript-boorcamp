d9d686f6409aa85c0677aebd3e9a083b6b10c68e
Mohamedabdelaziz142 <m.abdelaziz.hussen@gmail.com>
2026-04-23 11:17:33 +0200

    edited the errors in the atm file

--- tasks/atm.js
+++ tasks/atm.js
@@ -1,24 +1,31 @@
 let balance = 1000;
 let withdrawal = 500;
 
-if (balance < withdrawal)
+if (withdrawal <= 0) {
+  console.log("Invalid amount");
+}
+else if (balance < withdrawal)
   {
     console.log("Insufficient Balance");
   }
-else if(withdrawal % 100 !== 0){
-    console.log("ATM only despenses 100 and 200 notes")
-  }
+
 else{
-  let notes200 = Math.floor(withdrawal / 200);
-  let remainder = withdrawal % 200;
-  let note100 = 0;
-
-  if(remainder > 0){
-    note100 = remainder / 100;
+  withdrawal = Math.floor(withdrawal / 100) * 100
+  if (withdrawal <= 0) {
+    console.log("Invalid amount");
   }
-  balance-=withdrawal;
+ else{
+      let notes200 = Math.floor(withdrawal / 200);
+      let remainder = withdrawal % 200;
+      let notes100 = 0;
 
-console.log(`200 Notes: ${notes200} 100 Notes: ${notes100}`);
-console.log(`Remaining Balance: ${balance}`)
+      if(remainder > 0){
+        notes100 = remainder / 100;
+      }
+      balance-=withdrawal;
+
+    console.log(`200 Notes: ${notes200} 100 Notes: ${notes100}`);
+    console.log(`Remaining Balance: ${balance}`)
+ }
 }
 
